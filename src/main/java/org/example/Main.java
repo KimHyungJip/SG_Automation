a5c97b0d2d89c9858325a011c86e9c11dc30e42a
atath <atathj@nate.com>
2024-03-18 14:25:29 +0900

    v\필요없는 NameButton.java , UserInputProcessor.java 삭제
    main gui.java에서 끌고오게함

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,9 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            UserInputProcessor processor = new UserInputProcessor();
-            String[] credentials = processor.createAndShowGUI();
-            if (credentials != null && credentials.length == 2) {
-                // GUI 객체 생성
-                GUI gui = new GUI();
-                // SG_AutomationHandler 인스턴스 생성 시 GUI 객체 전달
-                SG_AutomationHandler handler = new SG_AutomationHandler(gui);
-                handler.processWebAutomation(credentials);
-            } else {
-                System.out.println("User canceled the input or an error occurred.");
-            }
+            // GUI 객체 생성
+            GUI gui = new GUI();
+            gui.setVisible(true); // GUI를 화면에 표시
         });
     }
 }
